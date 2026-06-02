2f866a08e292121b10ce9a7dbe643ca173114d16
JulanT0811 <jtlengua.9b@gmail.com>
2026-06-02 15:17:39 -0500

    02

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from soporte_tecnico.views.health import health_check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('soporte_tecnico.urls')),
+    path('health/', health_check),
 ]
